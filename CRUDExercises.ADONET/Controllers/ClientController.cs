42b348ac1bfd935bc7ffdc1317a4618123e75120
Mehdi MIEG <mmieg@diginamic-formation.fr>
2023-12-08 19:07:06 +0100

    Update: Controller logic

--- CRUDExercises.ADONET/Controllers/ClientController.cs
+++ CRUDExercises.ADONET/Controllers/ClientController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,11 +23,8 @@ internal class ClientController
 										string? postalCode = "",
 										string? city = "")
 	{
-		address = address == "" ? null : address;
-		postalCode = postalCode == "" ? null : postalCode;
-		city = city == "" ? null : city;
-
 		Client client = new(id: -1, firstName, lastName, birthDate, address, postalCode, city);
+		NullablePropertiesCheck(client);
 
 		try
 		{
@@ -84,11 +82,8 @@ internal class ClientController
 											string? postalCode = "",
 											string? city = "")
 	{
-		address = address == "" ? null : address;
-		postalCode = postalCode == "" ? null : postalCode;
-		city = city == "" ? null : city;
-
 		Client client = new(id: -1, firstName, lastName, birthDate, address, postalCode, city);
+		NullablePropertiesCheck(client);
 
 		Dictionary<string, dynamic?> parameters = RetrieveUpdateParameters(client);
 		//if (firstName != "") parameters.Add("Nom", firstName);
@@ -132,21 +127,37 @@ internal class ClientController
 
 
 	/// <summary>
-	/// Gets an object, tests all property types and values,
-	/// then returns a dictionnary of the necessary parameters for the update.
+	/// Switches nullable properties to null if their field is empty or default.
+	/// Could be done via reflection, but for the sake of the exercise...
 	/// </summary>
 	/// <param name="client"></param>
-	/// <returns>outputParams</returns>
+	private void NullablePropertiesCheck(Client client)
+	{
+		client.Adresse = client.Adresse == "" ? null : client.Adresse;
+		client.Code_Postal = client.Code_Postal == "" ? null : client.Code_Postal;
+		client.Ville = client.Ville == "" ? null : client.Ville;
+	}
+
+
+	/// <summary>
+	/// Gets an object, tests all property types and values,
+	/// then returns a dictionnary of the necessary parameters for the update.
+	/// Could be done via reflection, but for the sake of the exercise...
+	/// </summary>
+	/// <param name="client"></param>
+	/// <returns></returns>
 	private Dictionary<string, dynamic?> RetrieveUpdateParameters(Client client)
 	{
-
-
-		Dictionary<string, dynamic?> inputParams = new();
+		Dictionary<string, dynamic?> inputParams = new()
+		{
+			{"Nom", client.Nom},
+			{"Nom", client.Nom}
+		};
 		Dictionary<string, dynamic?> outputParams = new();
 
 		for (int i = 0; i < inputParams.Count; i++)
 		{
-			dynamic? value = inputParams.ElementAt(i).Value;
+			dynamic? value = client.GetType().GetProperties();
 
 			if (value is null)
 				outputParams.Add(inputParams.ElementAt(i).Key, null);
